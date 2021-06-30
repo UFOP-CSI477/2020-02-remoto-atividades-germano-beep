efd35196697cca3b3c69647ea5d078531b4e3173
arthur <garthur83@gmail.com>
2021-06-29 22:23:53 -0300

    modificaçoes calculadora

--- Atividades/atividade-pratica-01/02-calculadora.js
+++ Atividades/atividade-pratica-01/02-calculadora.js
@@ -1,5 +1,75 @@
 
 
-function seleciona(input){
-    document.getElementById("tela").value+= input.value;
+function seleciona(input) {
+    document.getElementById("tela").value += input.value;
+}
+
+function calcular() {
+    let operecao = document.getElementById("tela").value;
+    let auxiliarNum = "";
+    let resultado = 0;
+    let listOp = [];
+    let listNum = [];
+    let op;
+
+    for (let index = 0; index < operecao.length; index++) {
+
+        const element = operecao[index];
+        // n tá adicionando o último número
+
+        if (!isNaN(element)) {
+            auxiliarNum += element;
+
+        } else {
+
+            listNum.push((parseFloat(auxiliarNum)));
+            auxiliarNum = "";
+            op = element;
+            listOp.push(op);
+        }
+    }
+
+    listNum.push(parseFloat(auxiliarNum));
+
+    console.log(listNum);
+    console.log(listOp);
+
+    // tem que haver no mínimo dois num para uma operação
+    num1 = listNum.shift();
+    num2 = listNum.shift();
+    op = listOp.shift();
+
+    if (op == "+") {
+        resultado = num1+num2;
+    }
+    if (op == "-") {
+        resultado = num1-num2;
+    }
+    if (op == "*") {
+        resultado = num1*num2;
+    }
+    if (op == "/") {
+        resultado = num1/num2;
+    }
+
+    for (let index = 0; index < listNum.length;index++) {
+        num = listNum[index];
+        op = listOp[index]
+
+        if (op == "+") {
+            resultado +=num;
+        }
+        if (op == "-") {
+            resultado -=num;
+        }
+        if (op == "*") {
+            resultado *=num;
+        }
+        if (op == "/") {
+            resultado /=num;
+        }
+    
+    }
+
+    document.getElementById("tela").value = `${resultado}`;
 }
