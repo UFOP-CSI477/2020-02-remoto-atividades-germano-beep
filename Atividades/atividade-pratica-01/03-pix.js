dc5266d23a4e1e3ebfac5ed5e2c2022d5f3d11fc
arthur <garthur83@gmail.com>
2021-07-01 00:50:31 -0300

    adicionado relatorio de valor envio, recebimento e transaçoes

--- Atividades/atividade-pratica-01/03-pix.js
+++ Atividades/atividade-pratica-01/03-pix.js
@@ -1,5 +1,55 @@
+
+// object transacao
+function Transacao(tipo, valor) {
+    this.tipo = tipo;
+    this.valor = valor;
+}
+
+// armazena todas as transacoes
+let lista_transacoes = [];
+// confirmar cria um object tipo transacao
+
+function confirmar() {
+
+    let transacao;
+    let tipo = document.getElementById("tipo_operacao").value;
+    let valor = document.getElementById("valor").value;
+    if (tipo == "envio") {
+        transacao = new Transacao(tipo, -valor);
+    } else {
+        transacao = new Transacao(tipo, +valor);
+    }
+
+    lista_transacoes.push(transacao);
+    window.alert(`Transação concluída com sucesso`)
+}
+
+function relatorio_transacoes() {
+    console.log(lista_transacoes);
+
+    let envio = 0;
+    let recebimento = 0;
+    let saldo;
+
+    for (let index = 0; index < lista_transacoes.length; index++) {
+        const element = lista_transacoes[index];
+
+        if (element.tipo == "envio") {
+            envio += element.valor;
+        } else {
+            recebimento += element.valor;
+        }
+    }
+
+
+    saldo = recebimento + envio;
+    window.alert(`Envios: ${-envio}
+        Recebimentos: ${recebimento}
+        Saldo: ${saldo}`)
+}
+
 function limpar_select(campo) {
-    while(campo.length>1){
+    while (campo.length > 1) {
         campo.remove(1);
     }
 }
@@ -8,8 +58,8 @@ function preencherSelectBancos1(data) {
     let bancos = document.getElementById("bancos1");
     limpar_select(bancos);
 
-    for(let index in data){
-        const {isbp, code, fullName} = data[index];
+    for (let index in data) {
+        const { isbp, code, fullName } = data[index];
         let option = document.createElement("option");
 
         option.value = isbp;
@@ -24,8 +74,8 @@ function preencherSelectBancos2(data) {
     let bancos = document.getElementById("bancos2");
     limpar_select(bancos);
 
-    for(let index in data){
-        const {isbp, code, fullName} = data[index];
+    for (let index in data) {
+        const { isbp, code, fullName } = data[index];
         let option = document.createElement("option");
 
         option.value = isbp;
@@ -36,20 +86,20 @@ function preencherSelectBancos2(data) {
     }
 }
 
-function carregar_bancos1(){
+function carregar_bancos1() {
     fetch("https://brasilapi.com.br/api/banks/v1")
-    .then(response=>response.json())
-    .then(data=> preencherSelectBancos1(data))
-   
+        .then(response => response.json())
+        .then(data => preencherSelectBancos1(data))
 
-    .catch(error => console.error(error))
+
+        .catch(error => console.error(error))
 }
 
-function carregar_bancos2(){
+function carregar_bancos2() {
     fetch("https://brasilapi.com.br/api/banks/v1")
-    .then(response=>response.json())
-    .then(data=> preencherSelectBancos2(data))
-   
+        .then(response => response.json())
+        .then(data => preencherSelectBancos2(data))
 
-    .catch(error => console.error(error))
+
+        .catch(error => console.error(error))
 }
