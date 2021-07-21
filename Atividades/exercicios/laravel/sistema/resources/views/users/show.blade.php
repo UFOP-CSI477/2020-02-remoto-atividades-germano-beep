91d0ed2ed722b208f94366ad3f9e5599d4849065
arthur <garthur83@gmail.com>
2021-07-21 13:07:00 -0300

    add model para compra e views para usuário

---
+++ Atividades/exercicios/laravel/sistema/resources/views/users/show.blade.php
@@ -0,0 +1,25 @@
+@extends('principal')
+
+@section('conteudo')
+
+<h1>Dados do usuário</h1>
+
+<p>Id:{{$user->id}}</p>
+<p>nome:{{$user->name}}</p>
+<p>email:{{$user->email}}</p>
+
+
+<a class="btn btn-primary" href="{{route('users.index')}}">Voltar</a>
+
+
+<form name="frmDelete" action="{{route('users.destroy', $user->id)}}" method="POST" onsubmit="return confirm('Certeza que deseja excluir o usuário?')">
+    @csrf
+    @method('DELETE')
+
+    <input class="btn btn-danger" type="submit" value="Excluir">
+
+</form>
+
+
+
+@endsection
