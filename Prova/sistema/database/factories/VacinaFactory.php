711923d1275a41f74a4c8e1da35deca5f0d88266
arthur <garthur83@gmail.com>
2021-08-20 16:20:37 -0300

    add prova final

---
+++ Prova/sistema/database/factories/VacinaFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Vacina;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class VacinaFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Vacina::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
